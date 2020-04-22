685e0a74fcf80f041c536a16e9d115548ab9e20a
Matthias Jenny <matthiascjenny@gmail.com>
2020-04-22 10:31:37 -0400

    Reorganize file structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
-  entry: './entry.js',
+  entry: "./entry.js",
   output: {
-    filename: './script.js',
+    filename: "./dist/script.js",
   },
   module: {
     loaders: [
       {
         test: /\.worker\.js$/,
         exclude: /(node_modules)/,
-        loader: 'worker!babel?presets[]=env',
+        loader: "worker!babel?presets[]=env",
       },
     ],
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   resolve: {
-    extensions: ['.js', '.jsx', '*'],
+    extensions: [".js", ".jsx", "*"],
   },
 };
